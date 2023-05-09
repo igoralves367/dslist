3ad82c2447ba56d97c19d388964e8852ce778017
igoralves367 <igor.alves367@gmail.com>
2023-05-09 19:10:52 -0300

    Porject Created

---
+++ src/main/java/com/alvescompany/dslistalves/DslistAlvesApplication.java
@@ -0,0 +1,13 @@
+package com.alvescompany.dslistalves;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DslistAlvesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DslistAlvesApplication.class, args);
+	}
+
+}
